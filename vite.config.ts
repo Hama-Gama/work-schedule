d11677b39fb46cd68ec590ebe7b89c6fd00c0b27
Khamit <admiral3nelson7@gmail.com>
2025-12-18 22:32:09 +0500

    add avatar

--- vite.config.ts
+++ vite.config.ts
@@ -6,33 +6,35 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), 
+	plugins: [
+		react(),
 		VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg'],
-      manifest: {
-        name: 'My Offline App',
-        short_name: 'OfflineApp',
-        description: 'React app works offline',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    }),
-		tailwindcss()],
+			registerType: 'autoUpdate',
+			includeAssets: ['avatar.jpg', 'favicon.svg'],
+			manifest: {
+				name: 'My Offline App',
+				short_name: 'OfflineApp',
+				description: 'React app works offline',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone',
+				start_url: '/',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+		tailwindcss(),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
